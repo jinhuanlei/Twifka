12c26bc82558c0d4a558e4f2edfe86af23a36d3b
Jinhuan Lei <jlei@zendesk.com>
2019-12-20 14:58:19 -0600

    init Twifka

---
+++ src/main/java/Producers/TwitterProducer.java
@@ -0,0 +1,14 @@
+package Producers;
+
+public class TwitterProducer {
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+    public void run() {
+
+    }
+
+    public void createTwitterClient(){
+
+    }
+}
